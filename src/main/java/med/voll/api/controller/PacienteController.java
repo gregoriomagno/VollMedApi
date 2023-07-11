c4ec89ccdd31719c856e85c61452fc73e43906f7
Gregório Magno Neto <gregorio.neto@maida.health>
2023-07-10 22:42:23 -0300

    alterando confg

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,14 +1,15 @@
 package med.voll.api.controller;
 
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import med.voll.api.paciente.DadosCadastroPaciente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/paciente")
@@ -23,4 +24,10 @@ public class PacienteController {
         System.out.println("Cadastro paciente: "+ dados);
 
     }
+
+//    @GetMapping
+//    public Page<DadosListagemPaciente> listar(@PageableDefault (size=10, sort={"nome"} )Pageable paginacao){
+//
+//
+//    }
 }
